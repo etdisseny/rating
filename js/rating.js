ecef7b560b880ef7d72621a41fa05e8bc95b7302
Ester <estertermensprat@iMac.home>
2019-04-22 18:04:32 +0200

    rating

--- js/rating.js
+++ js/rating.js
@@ -1,53 +1,81 @@
 
 window.addEventListener('load', function () {
 
-    function cambiarColor(variable) {
-        variable.style.background = '#FFA208';
-    };
-    function recuperarColor(variable){
-        variable.style.background = '#25A681';
+
+    function cambiarColor(varColor) {
+        varColor.style.background = '#FFA208';
+    }
+    function cambiarIcono(varIco){
+        varIco.className += " happy";
     }
 
-    /*var icono = document.querySelectorAll('.far');
-    for (var i in icono) {
-        icono[i].classList.remove('fa-frown');
-        icono[i].classList.add('fa-grin');
-    }*/
-    function elementName(event){
-        alert(event.target.nodeName);
+    function recuperarColor(recColor){
+        recColor.style.background = '#25A681';
+       
     }
-    var body=document.querySelector('body');
+    function recuperarIcono(recIco){
+        recIco.className =recIco.className.replace( /(?:^|\s)happy(?!\S)/g , '' );
+    }
 
-    body.addEventListener('click', function(){
-        elementName(event);
-    });
 
     var elemento = document.querySelectorAll('.circle');
+    
+    var body=document.querySelector('body');
+  
 
-   for(let selec=0 ; selec<elemento.length; selec++) {
+    
+   
+   
+    for(let selec = 0; selec < elemento.length; selec++){
 
-    //Ejercicio 1- al estar encima cambia el color y al salir recupera el color
-      /* elemento[selec].addEventListener('mouseover', function(){
-            cambiarColor(this);
-       });
-      
-      elemento[selec].addEventListener('mouseout', function(){
-            recuperarColor(this);
-       });*/
      
-       
-    //al premer s'han de quedar fixe amb les anteriors
-        elemento[selec].addEventListener('click',function(){
-            while(selec>=0){
-                cambiarColor(elemento[selec]);
-                selec--;
-               
-            }
+        /* elemento[selec].addEventListener('mouseover', function(){
+            cambiarIcono(this);
+
         });
 
-    };
-        
+        elemento[selec].addEventListener('mouseout', function(){
+            recuperarIcono(this);
+        }); */
+           
+                      
+                
+            
+       
+        //al premer s'han de quedar fixe amb les anteriors
+            elemento[selec].addEventListener('click', function() {
+               
+                var elementName= event.target.className; //recupero el className del elemento al que hago clic
+            
+                if(elementName=="circle"){
+                    for(let i = 0; i<elemento.length; i++){
+                        recuperarColor(elemento[i]);  
+                    }
+                    
+                } 
+                
+                for(let i = 0; i<=selec; i++){
+                    cambiarColor(elemento[i]);  
+                }
+                 
+            });
 
+        //al clicar fuera recuperamos el estilo inicial
+            body.addEventListener('click', function(event){
+                var elementName= event.target.className; //recupero el className del elemento al que hago clic
+            
+                if(elementName!="circle"){
+                    recuperarColor(elemento[selec]);
+                } 
+
+            });
+
+
+            
+           
+   };//for
+
+                    
 
   
 
